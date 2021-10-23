1c010aa6fd54ae2d9c18f1388d4af817545d7a1e
Vadim Koritskiy <vadim.koritskij.01@privatbank.ua>
2021-10-23 09:01:46 +0300

    init commit

---
+++ src/com/pb/Koritskiy/hw3/Bingo.java
@@ -0,0 +1,54 @@
+package com.pb.Koritskiy.hw3;
+import java.util.Scanner;
+public class Bingo {
+    public static void main(String[] args) {
+        System.out.println("Отгадайте число которое задумала программа \"Bingo\" в диапазоне от 0 до 100 ");
+        System.out.println("Если хотите выйти введите \"exit\"");
+        final int max= 100;
+        int attempt =0;
+        int number = 33;
+        Scanner in = new Scanner(System.in);
+
+        while (attempt<max){
+            attempt++;
+            System.out.println("Попытка №" + attempt + ":");
+            String user = in.next();
+
+            if (user.equals("exit")){
+                System.out.println("Попробуйте в следующий раз Arividerchi");
+                break;
+            }
+            if (Integer.parseInt(user) > 100){
+                System.out.println("Вы ввели число больше чем 100 ");
+                break;
+
+            }
+            if (Integer.parseInt(user) < 0){
+                System.out.println("Вы ввели число меньше 0 ");
+                break;
+            }
+
+            if (Integer.parseInt(user) == number) {
+                System.out.println("Поздравляем, Вы угадали с " + attempt + " попытки!");
+                break;
+            }
+
+            if (Integer.parseInt(user) > number) {
+                System.out.println("Загаданное число меньше " + user);
+            } else {
+                if (Integer.parseInt(user) >= number) {
+                    System.out.println("Поздравляем, Вы угадали с " + attempt + " попытки!");
+                    break;
+                }
+
+                System.out.println("Загаданное число больше " + user);
+            }
+        }
+        System.out.println("Конец игры");
+
+
+
+
+
+    }
+}
